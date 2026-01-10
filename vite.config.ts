c16897ba2e22690f6aaa5b7f1b14bf6cbb19a8b0
John J. Camilleri <john@johnjcamilleri.com>
2026-01-10 14:28:51 +0100

    Remove devtools

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import path from 'node:path'
 
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
-// import vueDevTools from 'vite-plugin-vue-devtools'
 import ViteYaml from '@modyfi/vite-plugin-yaml';
 
 // https://vite.dev/config/
